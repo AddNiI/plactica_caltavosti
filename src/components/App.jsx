eb2112f3b369b7e5d7b557e858651ff7b9fb388b
AddNiI <100078200+AddNiI@users.noreply.github.com>
2025-03-21 14:33:19 +0200

    0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,21 @@
+import React from "react";
+import initialTodos from "../todo.json";
+import TodoList from "./TodoList"
 
+export class App extends React.Component{
+  state={
+    todos: initialTodos,
+    filter: '',
+  }
 
-state={
-  todos: initialTodos,
-  filter: '',
+  deleteTodo=(id)=>{
+    this.setState((prevState)=>({todos: prevState.todos.filter((todo)=>todo.id !== id)}))}
+
+  render(){
+    return<>
+    <TodoList 
+    onDelete = {this.deleteTodo}
+    todos={this.state.todos} />
+    </>
+  }
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+function TodoList({todos, onDelete}) { 
+    return <ul>
+        {todos.map((todo)=>{
+            return <li key={todo.id}>
+            <label>
+                    <input type="checkbox" name="agree" checked={todo.completed} />
+            </label>
+            <p>{todo.text}</p>
+            <button onClick={() => {onDelete(todo.id)}} typr="button">Видалити</button>
+        </li>})}
+    </ul>
+    return
+}
+
+export default TodoList
